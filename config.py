cd7552a4638956fc7bef7594a91b4273e54b69d8
sanaullah-creator <lsanau860@gmail.com>
2026-05-09 14:39:10 +0500

    database config change

--- config.py
+++ config.py
@@ -4,10 +4,19 @@ class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY', 'mysecretkey')
 
-    MYSQL_HOST = 'localhost'
+    MYSQL_HOST = 'sql312.infinityfree.com'
 
-    MYSQL_USER = 'root'
+    MYSQL_USER = 'if0_41847455'
 
-    MYSQL_PASSWORD = ''
+    MYSQL_PASSWORD = 'I9BB771jqjT'
 
-    MYSQL_DB = 'student_performance_db'
+    MYSQL_DB = 'if0_41847455_studentperformance'
+    # SECRET_KEY = os.environ.get('SECRET_KEY', 'mysecretkey')
+
+    # MYSQL_HOST = 'localhost'
+
+    # MYSQL_USER = 'root'
+
+    # MYSQL_PASSWORD = ''
+
+    # MYSQL_DB = 'student_performance_db'
